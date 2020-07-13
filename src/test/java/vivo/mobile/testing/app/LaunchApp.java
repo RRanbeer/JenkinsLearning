da5f25016d17795dd431698d3615170e4c9f3193
RRanbeer <rajaranbeersingh786@gmail.com>
2020-07-14 03:43:49 +0530

    First

---
+++ src/test/java/vivo/mobile/testing/app/LaunchApp.java
@@ -0,0 +1,21 @@
+package vivo.mobile.testing.app;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchApp 
+{
+	
+	@Test
+	public void test_LaunchApp()
+	{
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.google.com/");
+	
+	System.out.println("Title of the WebPage --> ");
+	
+	}
+}
